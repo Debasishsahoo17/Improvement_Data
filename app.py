37f9cbb072f5a13888134a4d8a0e657690b976fe
Debasish Sahoo <debasish172003@gmaill.com>
2025-02-11 01:01:58 +0530

    Updated app.py with fixes

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def search_student():
     return jsonify(students)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render ke environment variable se port lega
+    app.run(host="0.0.0.0", port=port, debug=True)
